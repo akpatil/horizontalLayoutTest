1bbab256abdbd8c255652aa2cb8a36869993510a
akshaypatilofficial <akshaypatilofficial93@gmail.com>
2015-10-03 20:21:04 +0530

    initial commit

---
+++ config/express.js
@@ -0,0 +1,31 @@
+var config         = require('./config');
+var express        = require('express'),
+	morgan         = require('morgan'),
+	compress       = require('compression'),
+	bodyParser     = require('body-parser'),
+	methodOverride = require('method-override');
+
+module.exports = function() {
+	var app = express();
+
+	if(process.env.NODE_ENV === 'development'){
+		app.use(morgan('dev'));
+	}
+	else if(process.env.NODE_ENV === 'production') {
+		app.use(compress());
+	}
+
+	app.use(bodyParser.urlencoded({
+		extended: true
+	}));
+	app.use(bodyParser.json());
+	app.use(methodOverride());
+
+	app.set('views', './app/views');
+	app.set('view engine', 'ejs');
+
+	require('../app/routes/index.server.routes')(app);
+	require('../app/routes/users.server.routes')(app);
+	app.use(express.static('./public'));
+	return app;
+};
